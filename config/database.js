e32f3b13aba0e3e69396dee7cfd94aebd7244fac
HemaEzzat123 <hezzat.964@gmail.com>
2025-07-31 17:58:05 +0300

    first commit

--- config/database.js
+++ config/database.js
@@ -4,13 +4,12 @@ const connectDB = async () => {
   try {
     const conn = await mongoose.connect(
       process.env.MONGODB_URI ||
-        "mongodb+srv://ecosus:Aa12345@@ecosus.zbf9vge.mongodb.net/ecosus",
+        "mongodb+srv://Eco:Aa12345@ecosus.zbf9vge.mongodb.net/ecosus",
       {
         useNewUrlParser: true,
         useUnifiedTopology: true,
       }
     );
-
   } catch (error) {
     console.error(`Error: ${error.message}`);
     process.exit(1);

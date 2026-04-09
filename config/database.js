e1bbb666daec4cf469f22d733334e93462bd2f87
ecosus <ecosus48@gmail.com>
2026-04-09 14:14:37 +0200

    Update database.js

--- config/database.js
+++ config/database.js
@@ -3,11 +3,12 @@ const mongoose = require("mongoose");
 const connectDB = async () => {
   try {
     const conn = await mongoose.connect(
-        "mongodb://Eco:iW3o15X0ChPN4YZ0@ac-4mreb6j-shard-00-00.zbf9vge.mongodb.net:27017,ac-4mreb6j-shard-00-01.zbf9vge.mongodb.net:27017,ac-4mreb6j-shard-00-02.zbf9vge.mongodb.net:27017/ecosus?ssl=true&replicaSet=atlas-1x7uma-shard-0&authSource=admin&retryWrites=true&w=majority",
-      
+      "mongodb+srv://Eco:GST9WE4PodAk1kf7@ecosus.zbf9vge.mongodb.net/ecosus?retryWrites=true&w=majority&tls=true"
       {
         useNewUrlParser: true,
         useUnifiedTopology: true,
+        family: 4
+
       }
     );
   } catch (error) {
